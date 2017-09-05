e75b49feb64d70072e19018c859ab9864f0f1ee2
Reinout Bossuyt <reinout@cultuurnet.be>
2017-09-05 13:25:37 +0200

    revert previous changes

--- web/index.php
+++ web/index.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use CultuurNet\MailMicroservice\Controller\ContactListsControllerProvider;
 use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
 
 /** @var Application $app */
-$app = require __DIR__ . '/bootstrap.php';
+$app = require __DIR__ . '/../bootstrap.php';
 
 $app["cors-enabled"]($app);
 
